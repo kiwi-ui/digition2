95cce910c5661a0194412bef1ca6b2d40b09a8fe
kiwi-ui <yusnianggara@gmail.com>
2023-09-10 17:20:07 +0700

    fix: fix layuot button color and responsiveness

--- src/components/ModalImage/index.jsx
+++ src/components/ModalImage/index.jsx
@@ -12,7 +12,7 @@ const ModalImage = () => {
         <>
             {
                 isPhotoClicked ? 
-                    <div className={ `${ style.bgDark50 } position-absolute h-100 z100 w-100 row justify-content-center align-items-center` }>        
+                    <div className={ `${ style.bgDark50 } position-absolute vh-100 z100 w-100 row justify-content-center align-items-center` }>        
                         <img src={ clickedPhoto } className="col-lg-6 col-10 position-relative z100 " alt="prewedding" /> 
                         <button className="bg-transparent fs-4 position-absolute rounded-circle px-3 pt-3 top-0 text-white border-0 fw-bold d-flex justify-content-end" onClick={closeModal} style={{zIndex: 10}}><AiOutlineClose className='text-white'/></button>
                     </div> : null
