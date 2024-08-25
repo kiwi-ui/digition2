3fe38dff6709dec37271e684ed0babc7e5210ac1
yusni anggara <yusnianggara@gmail.com>
2024-08-25 10:14:34 +0700

    style: style page1 fixed

--- src/pages/Page1/index.jsx
+++ src/pages/Page1/index.jsx
@@ -4,9 +4,8 @@ import {BsInstagram} from 'react-icons/bs';
 import style from './index.module.css';
 
 const Page1 = () => {
-
   return (
-		<section className="text-center container">
+		<section className="text-center container my-5  pb-5">
 			<p className="fw-semibold fs-3 dancing-font">Assalamual aikum Wr Wb</p>
 			<p className="mb-0">Dengan rahmat ridho Allah <i>Subhanahu Wa Ta'ala,</i> kami menyelenggarakan acara resepsi pernikahan putra-putri kami:</p>
 
@@ -14,11 +13,11 @@ const Page1 = () => {
 				<div className="col-lg-4 my-2">
 					<img src={bride} className={`img-fluid rounded-4 style['w-85'] shadow`} alt="bride"/>
 
-					<div className={`${style['card-details']} container text-white rounded-4 py-4 position-relative shadow border border-3 border-white`} style={{marginTop: '-3rem', zIndex:10, width:'90%' }}>
-						<p className="fs-2 fw-semibold dancing-font mb-0">Putri Ayuningtyas</p>
+					<div className={`${style['card-details']} container  rounded-4 py-4 position-relative shadow border border-3 border-white`} style={{marginTop: '-3rem', zIndex:10, width:'90%' }}>
+						<p className="fs-4 fw-semibold mb-2">Putri Ayuningtyas</p>
 						<p className="m-0">Putri dari Bapak Ahmad Hidayat &amp; Ibu Nurul Rahayu</p>
-						<button className={`text-white btn  mt-2 bg-white rounded-circle mt-4`} href="https://www.instagram.com">
-							<a href="https://www.instagram.com/yusnianggara target='__blank'"><BsInstagram  className="mb-1 text-black position-relative" style={{color: 'rgba(164, 158, 153)'}}/></a>
+						<button className={` btn  rounded-circle mt-4`} href="https://www.instagram.com">
+							<a href="https://www.instagram.com/yusnianggara target='__blank'"><BsInstagram  className="position-relative  fw-bold fs-1" color='#dfc7ac'/></a>
 						</button>
 					</div>
 				</div>
@@ -26,18 +25,14 @@ const Page1 = () => {
 				<div className="col-lg-4 my-2">
 					<img src={groom} className={`img-fluid rounded-4 style['w-85'] shadow`} alt="goorm"/>
 					
-					<div className={`${style['card-details']} container text-white rounded-4 py-4 position-relative shadow border border-3 border-white`} style={{marginTop: '-3rem', zIndex:10, width:'90%' }}>
-						<p className="fs-3 fw-semibold dancing-font mb-0">Baim Ramadhan</p>
+					<div className={`${style['card-details']} container  rounded-4 py-4 position-relative shadow border border-3 border-white`} style={{marginTop: '-3rem', zIndex:10, width:'90%' }}>
+						<p className="fs-4 fw-semibold mb-2">Baim Ramadhan</p>
 						<p className="m-0">Putra dari Bapak Hadi Kusuma &amp; Ibu Anita Rahmawati</p>
-						<button className={`text-white btn  mt-2 bg-white rounded-circle mt-4`} href="https://www.instagram.com">
-							<a href="https://www.instagram.com/divit.ion/" target='__blank'><BsInstagram  className="mb-1 position-relative" style={{color: 'rgba(164, 158, 153)'}}/></a>
+						<button className={` btn  rounded-circle mt-4`} href="https://www.instagram.com">
+							<a href="https://www.instagram.com/yusnianggara target='__blank'"><BsInstagram  className="position-relative  fw-bold fs-1" color='#dfc7ac'/></a>
 						</button>
 					</div>
 				</div>
-			</div>
-
-			<div>
-				<p>Untuk melaksanakan syari'at agama dan menjalankan sunnah Rasul dalam membina keluarga yang sakinah, mawaddah warahmah</p>
 			</div>
 		</section>
   );
